68824fe930f6bd224cac2a31ff48774d4f5523db
Hellmar Becker <becker@hellmar-becker.de>
2023-06-22 09:25:24 +0200

    Add Geo headers

--- send_kafka.sh
+++ send_kafka.sh
@@ -5,8 +5,17 @@ CC_APIKEY="<api key>"
 CC_SECRET="<secret>"
 CC_SECURE="-X security.protocol=SASL_SSL -X sasl.mechanism=PLAIN -X sasl.username=${CC_APIKEY} -X sasl.password=${CC_SECRET}"
 CLIENT_ID="<client id>"
+LON="0.0"
+LAT="0.0"
 TOPIC_NAME="adsb-raw"
 
 nc localhost 30003 \
     | awk -F "," '{ print $5 "|" $0 }' \
-    | kafkacat -P -t ${TOPIC_NAME} -b ${CC_BOOTSTRAP} -H "ClientID=${CLIENT_ID}" -K "|" ${CC_SECURE}
+    | kafkacat -P \
+        -t ${TOPIC_NAME} \ 
+        -b ${CC_BOOTSTRAP} \
+        -H "ClientID=${CLIENT_ID}" \
+        -H "ReceiverLon=${LON}" \
+        -H "ReceiverLat=${LAT}" \
+        -K "|" \
+        ${CC_SECURE}
