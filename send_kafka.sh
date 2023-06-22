2e0be57121cbea7db1b474a23ece2d7524b345e9
Hellmar Becker <becker@hellmar-becker.de>
2023-06-22 09:21:22 +0200

    Add Client ID header

--- send_kafka.sh
+++ send_kafka.sh
@@ -4,8 +4,9 @@ CC_BOOTSTRAP="<confluent cloud bootstrap server>"
 CC_APIKEY="<api key>"
 CC_SECRET="<secret>"
 CC_SECURE="-X security.protocol=SASL_SSL -X sasl.mechanism=PLAIN -X sasl.username=${CC_APIKEY} -X sasl.password=${CC_SECRET}"
+CLIENT_ID="<client id>"
 TOPIC_NAME="adsb-raw"
 
 nc localhost 30003 \
     | awk -F "," '{ print $5 "|" $0 }' \
-    | kafkacat -P -t ${TOPIC_NAME} -b ${CC_BOOTSTRAP} -K "|" ${CC_SECURE}
+    | kafkacat -P -t ${TOPIC_NAME} -b ${CC_BOOTSTRAP} -H "ClientID=${CLIENT_ID}" -K "|" ${CC_SECURE}
