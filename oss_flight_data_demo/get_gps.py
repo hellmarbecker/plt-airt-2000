03f4f98bfd4c8704545f3ac27c9e577f1ef8697a
Hugh Evans <hugh.evans.dev@gmail.com>
2024-06-07 15:23:31 +0100

    Don't write GPS data to file until we get a read

--- oss_flight_data_demo/get_gps.py
+++ oss_flight_data_demo/get_gps.py
@@ -3,9 +3,14 @@ from pigps import GPS
 from time import sleep
 gps = GPS()
 sleep(30)
-with open("/home/admin/time.txt","w") as file:
-    file.write(str(gps.time))
-with open("/home/admin/lat.txt","w") as file:
-    file.write(str(gps.lat))
-with open("/home/admin/lon.txt","w") as file:
-    file.write(str(gps.lon))
+if gps.time != "0":
+    with open("/home/admin/time.txt","w") as file:
+        file.write(str(gps.time))
+
+if gps.lat != "0":
+    with open("/home/admin/lat.txt","w") as file:
+        file.write(str(gps.lat))
+
+if gps.lon != "0":
+    with open("/home/admin/lon.txt","w") as file:
+        file.write(str(gps.lon))
