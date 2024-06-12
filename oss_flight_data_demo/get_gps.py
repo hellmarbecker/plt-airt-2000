074b69bc37eda3fa36035c61b21d2fd4b1b0a001
Hugh Evans <hugh.evans.dev@gmail.com>
2024-06-12 14:50:19 +0100

    Readded time delay before reading from GPS

--- oss_flight_data_demo/get_gps.py
+++ oss_flight_data_demo/get_gps.py
@@ -1,14 +1,16 @@
-from pigps import GPS # type: ignore
+from pigps import GPS
 from time import sleep
 gps = GPS()
+sleep(5)
+print(gps.time,gps.lat,gps.lon)
 if gps.time != "0":
     with open("/home/admin/time.txt","w") as file:
         file.write(str(gps.time))
 
-if gps.lat != "0":
+if gps.lat != "0.0":
     with open("/home/admin/lat.txt","w") as file:
         file.write(str(gps.lat))
 
-if gps.lon != "0":
+if gps.lon != "0.0":
     with open("/home/admin/lon.txt","w") as file:
         file.write(str(gps.lon))
