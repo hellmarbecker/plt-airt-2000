65ca55952a7567ab51ec1d93a899dc10880e7dae
Hugh Evans <hugh.evans.dev@gmail.com>
2024-06-12 15:20:34 +0100

    Fixed null condition handling in GPS script

--- oss_flight_data_demo/get_gps.py
+++ oss_flight_data_demo/get_gps.py
@@ -2,15 +2,17 @@ from pigps import GPS
 from time import sleep
 gps = GPS()
 sleep(5)
-print(gps.time,gps.lat,gps.lon)
-if gps.time != "0":
+if gps.time != 0:
     with open("/home/admin/time.txt","w") as file:
+        print("Got gps time", gps.time)
         file.write(str(gps.time))
 
-if gps.lat != "0.0":
+if gps.lat != 0:
     with open("/home/admin/lat.txt","w") as file:
+        print("Got gps lat", gps.lat)
         file.write(str(gps.lat))
 
-if gps.lon != "0.0":
+if gps.lon != 0:
     with open("/home/admin/lon.txt","w") as file:
+        print("Got gps lon", gps.lon)
         file.write(str(gps.lon))
